8d076a98aab8a209b21c43f8ead286f39e453baa
Syn Zhang <zeyanzhang.china@gmail.com>
2022-04-05 23:16:36 +0800

    feat: build my own State Machine

---
+++ state-machine/index.ts
@@ -0,0 +1,27 @@
+function createMachine (stateMachineDefinition) {
+  const machine = {
+    value: stateMachineDefinition.initialState,
+    transition (currentState, event) {
+      const currentStateDefinition = stateMachineDefinition[currentState]
+      const destinationTransition = currentStateDefinition.transitions[event]
+
+      if (!destinationTransition) {
+        return
+      }
+
+      const destinationState = destinationTransition.target
+      const destinationStateDefinition = stateMachineDefinition[destinationState]
+
+      destinationTransition.action()
+      currentStateDefinition.actions.onExit()
+      destinationStateDefinition.actions.onEnter()
+      machine.value = destinationState
+
+      return machine.value
+    },
+  }
+
+  return machine
+}
+
+export default createMachine
